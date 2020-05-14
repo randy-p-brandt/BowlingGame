92ebfac06a85b915e865206386ac55e01d2254d7
tallenqualtraxcom <tallen@qualtrax.com>
2020-05-14 15:39:38 -0400

    v1.0 Initial solution

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,35 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int s = 0;
+            int i = 0;
+
+            for (int f = 0; f < 10; f++)
+            {
+                if (rolls[i] + rolls[i + 1] == 10)
+                {
+                    s += 10 + rolls[i + 2];
+                }
+                else
+                {
+                    s += rolls[i] + rolls[i + 1];
+                }
+
+                i = i + 2;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ BowlingGameTests/GameTests.cs
@@ -0,0 +1,50 @@
+﻿using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void TestAllGutterballs()
+        {
+            RollMany(20, 0);
+
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollMany(17,0);
+            
+            Assert.AreEqual(16,game.Score());
+        }
+        
+        private void RollMany(int numberOfRolls, int pins)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+                game.Roll(pins);
+        }
+    }
+}
